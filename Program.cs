e6f48a219c2cc325457d40f79d26c84e6816ac28
marxjoseph <marquesjoseph03@gmail.com>
2024-07-04 23:30:13 -0400

    Comments

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MySite;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<App>("#app"); // Adds App.razor as a root component with an id of app
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
