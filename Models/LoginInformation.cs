64f1264f33df50320ed3e397c863913375ca0022
marxjoseph <marquesjoseph03@gmail.com>
2024-07-07 01:43:34 -0400

    Login Modal Completed

---
+++ Models/LoginInformation.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MySite.Models;
+
+public class LoginInformation
+{
+    [Required(ErrorMessage = "The username or password you entered does not exist.")]
+    public string Username { get; set; } = string.Empty;
+    [Required]
+    public string Password { get; set; } = string.Empty;
+}
